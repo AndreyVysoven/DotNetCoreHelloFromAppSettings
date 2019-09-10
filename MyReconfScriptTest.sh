56f0e95a22386544e28a0e501048dc4befa36cf0
Andrii Vysoven <aisq@ukr.net>
2019-09-10 18:58:59 +0300

    MyScript

--- MyReconfScriptTest.sh
+++ MyReconfScriptTest.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+#echo $Hello
+#echo $HelloValue
 
-cd /app
-if [ -n "$HelloValue" ]; then
-    cat <<EOT>> appsettings.json
-    {
-        "HelloValue": "${HelloValue}"  
-    } 
-elif [ -z "$HelloValue" ]; then
-    echo ${HelloValue}
+T1="$HelloValue"
+T2="$HelloEnv"
+        if [ "$T1" != "$T2" ]; then
+dotnet /app/src/DotNetCoreHelloFromAppSettings/test1/DotNetCoreHelloFromAppSettings.dll
+fi 
+if [ "$T1" == "$T2" ]; then
+cd /app/src/DotNetCoreHelloFromAppSettings/default/ && dotnet DotNetCoreHelloFromAppSettings.dll
 fi
-
-
-cd ./
-dotnet DotNetCoreHelloFromAppSettings.dll
 
